85166ff802b25340ad009e4bb5ee9bc75b72435e
trungmt <trungtran.cs@gmail.com>
2021-07-31 03:38:23 +0700

    auth middleware with Express's Request extends by Typescript Declaration Merging

---
+++ src/middleware/auth.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+import User, { IUserJson } from '../model/user';
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const accessToken = req.get('Authorization')?.replace('Bearer', '');
+    if (!accessToken) {
+      throw new Error();
+    }
+
+    const userJSON = jwt.verify(
+      accessToken!,
+      process.env.ACCESS_TOKEN_SECRET!
+    ) as IUserJson;
+    const user = User.findOne({
+      _id: userJSON._id,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.token = accessToken;
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send();
+  }
+};
+
+export default auth;
