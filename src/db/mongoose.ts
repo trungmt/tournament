d7b299892138c6bfea25b66e80959bde599a5bbb
trungmt <trungtran.cs@gmail.com>
2021-09-10 03:29:31 +0700

    change util directory into services directory

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { MONGOOSE_CONNECTION_URL } from '../util/secret';
+import { MONGOOSE_CONNECTION_URL } from '../services/secret';
 
 mongoose.connect(MONGOOSE_CONNECTION_URL!, {
   useNewUrlParser: true,
