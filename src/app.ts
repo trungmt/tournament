a5da8df65fff5b4c63b66de630c277b638cf3e3d
trungmt <trungtran.cs@gmail.com>
2021-09-26 02:07:45 +0700

    add cron for temp file garbage collector

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cron from 'node-cron';
 import authRouter from './routers/auth/auth';
 import adminRouter from './routers/admin/admin';
 import { logException, responseException } from './middlewares/exception';
-require('./db/mongoose');
+import { removeOldTempFiles } from './middlewares/upload';
+require('./db/mongoose'); // move to providers/
 
 const app = express();
+
+// TODO: make it a provider class that inject response function to app object
+// same as https://github.com/GeekyAnts/express-typescript/blob/7bb6ac09ca0270cb82a1730d9bb8d6efb89c999e/src/middlewares/Kernel.ts
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
@@ -17,5 +22,7 @@ app.get('/', (req, res) => {
 
 app.use(logException);
 app.use(responseException);
+
+cron.schedule('57 1 * * *', async () => removeOldTempFiles(86400000));
 
 export default app;
