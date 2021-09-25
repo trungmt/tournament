71bb8cf49e7730218ee2a1502d8f61abdcbf39e2
trungmt <trungtran.cs@gmail.com>
2021-09-26 02:02:20 +0700

    add label for team validation rule

--- src/validations/teamValidationSchema.ts
+++ src/validations/teamValidationSchema.ts
@@ -8,19 +8,18 @@ export interface ITeamFileForm {
   flagIcon: Express.Multer.File;
 }
 
-// TODO: leverage validation with label
-// TODO: leverage validation with languages
-// (message function - https://github.com/sideway/joi/blob/83092836583a7f4ce16cbf116b8776737e80d16f/test/base.js#L1920)
+// NOTE:(message function - https://github.com/sideway/joi/blob/83092836583a7f4ce16cbf116b8776737e80d16f/test/base.js#L1920)
 export const teamFieldValidationSchema = joi.object<ITeamFieldForm, true>({
   name: joi.string().required().label('Name'),
   permalink: joi
     .string()
     .required()
-    .pattern(/^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/),
+    .pattern(/^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/)
+    .label('Permalink'),
 });
 
 export const teamFileValidationSchema = joi.object<ITeamFileForm, true>({
-  flagIcon: joi.object().required(),
+  flagIcon: joi.object().required().label('Flag Icon'),
 });
 
 export const options: ValidationOptions = {
