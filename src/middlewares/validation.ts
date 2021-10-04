4f4737da0a145889f939a849c706e635958b0400
trungmt <trungtran.cs@gmail.com>
2021-10-04 09:54:13 +0700

    joi: add validationAsync middleware for cases there are validation rules run asynchronously with specified with joi's external()

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,7 +1,11 @@
-import Joi from 'joi';
+import Joi, { ValidationOptions } from 'joi';
 import { RequestHandler, Request, Response, NextFunction } from 'express';
-import { options } from '../validations/teamValidationSchema';
 import { ValidationError } from '../exceptions/ValidationError';
+
+const options: ValidationOptions = {
+  abortEarly: false,
+  allowUnknown: true,
+};
 
 export const validation =
   (schema: Joi.ObjectSchema): RequestHandler =>
@@ -26,4 +30,32 @@ export const validation =
       req.errors
     );
     next(validationError);
+  };
+
+export const validationAsync =
+  (schema: Joi.ObjectSchema): RequestHandler =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    if (!req.errors) req.errors = {};
+
+    try {
+      const value = await schema.validateAsync(req.body, options);
+      next();
+      return;
+    } catch (error) {
+      console.log('ERROR', error);
+      if (error instanceof Joi.ValidationError) {
+        error.details.forEach(err => {
+          const fieldName = err.path.pop()?.toString();
+          if (fieldName && req.errors && !req.errors[fieldName]) {
+            req.errors[fieldName] = err.message;
+          }
+        });
+      }
+      const validationError = new ValidationError(
+        'There are input validation errors. Please check each input again base on our instruction.',
+        req.errors
+      );
+      next(validationError);
+    }
   };
