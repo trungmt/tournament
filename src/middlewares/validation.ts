7cf0d2ce631476aeed7440d8b641125992ff7651
trungmt <trungtran.cs@gmail.com>
2021-10-09 00:15:04 +0700

    switch validation library from joi to yup, because joi prioritizes sync validation over async (if sync validation failed, not run async) add custom validation for team's name, permalink and flagIcon path

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,52 +1,34 @@
-import Joi, { ValidationOptions } from 'joi';
+// import Joi, { ValidationOptions } from 'joi';
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 import { ValidationError } from '../exceptions/ValidationError';
+import yup, {
+  AnyObjectSchema,
+  ValidationError as yupValidationError,
+} from 'yup';
 
-const options: ValidationOptions = {
+const options = {
   abortEarly: false,
   allowUnknown: true,
 };
 
-export const validation =
-  (schema: Joi.ObjectSchema): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) => {
-    if (!req.errors) req.errors = {};
-
-    const { error, value } = schema.validate(req.body, options);
-    if (!error) {
-      next();
-      return;
-    }
-
-    error!.details.forEach(err => {
-      const fieldName = err.path.pop()?.toString();
-      if (fieldName && req.errors && !req.errors[fieldName]) {
-        req.errors[fieldName] = err.message;
-      }
-    });
-
-    const validationError = new ValidationError(
-      'There are input validation errors. Please check each input again base on our instruction.',
-      req.errors
-    );
-    next(validationError);
-  };
-
 export const validationAsync =
-  (schema: Joi.ObjectSchema): RequestHandler =>
+  (schema: AnyObjectSchema): RequestHandler =>
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
     if (!req.errors) req.errors = {};
+    const _id = req.params.id;
 
     try {
-      const value = await schema.validateAsync(req.body, options);
+      await schema.validate(req.body, {
+        ...options,
+        context: { next, _id },
+      });
       next();
       return;
     } catch (error) {
-      console.log('ERROR', error);
-      if (error instanceof Joi.ValidationError) {
-        error.details.forEach(err => {
-          const fieldName = err.path.pop()?.toString();
+      console.log('Error validationAsync', error);
+      if (error instanceof yupValidationError) {
+        error.inner.forEach(err => {
+          const fieldName = err.path?.toString();
           if (fieldName && req.errors && !req.errors[fieldName]) {
             req.errors[fieldName] = err.message;
           }
--- src/validations/teamValidationSchema.ts
+++ src/validations/teamValidationSchema.ts
@@ -1,12 +1,9 @@
 import path from 'path';
 import { access } from 'fs/promises';
-import joi, {
-  CustomValidator,
-  ExternalValidationFunction,
-  ValidationOptions,
-} from 'joi';
-import Joi from 'joi';
-
+import Team, { ITeamDoc } from '../models/team';
+import BaseError from '../exceptions/BaseError';
+import { Document, FilterQuery } from 'mongoose';
+import { object, string, mixed, TestFunction, SchemaOf } from 'yup';
 export interface ITeamFieldForm {
   name: string;
   permalink: string;
@@ -14,54 +11,150 @@ export interface ITeamFieldForm {
 }
 
 export interface ITeamFileForm {
-  flagIcon: Express.Multer.File;
+  flagIcon?: Express.Multer.File;
 }
 
-const flagIconFilePathValidation: ExternalValidationFunction = async value => {
-  const tempFilePath = path.join(
-    process.env.UPLOAD_TEMP_FILE_DIR!,
-    'teams',
-    value
-  );
-
-  try {
-    await access(tempFilePath);
-  } catch (error) {
-    throw new Joi.ValidationError(
-      'Invalid Flag Icon file path',
-      [
-        {
-          message: 'Invalid Flag Icon file path',
-          path: ['flagIcon'],
-          type: 'any.external',
-          context: {
-            key: 'flagIcon',
-            label: 'Flag Icon',
-            value,
-          },
-        },
-      ],
+const flagIconFilePathValidation: TestFunction<string | undefined> =
+  async function (this, value) {
+    if (typeof value === 'undefined') {
+      return true;
+    }
+    const tempFilePath = path.join(
+      process.env.UPLOAD_TEMP_FILE_DIR!,
+      'teams',
       value
     );
+
+    try {
+      await access(tempFilePath);
+    } catch (error) {
+      return false;
+    }
+    return true;
+  };
+
+const duplicatePermalinkValidation: TestFunction<
+  string | undefined,
+  Record<string, any>
+> = async function (this, permalink) {
+  if (typeof permalink === 'undefined') {
+    return true;
   }
-  return value;
+
+  let team: (ITeamDoc & Document<any, any, ITeamDoc>) | null;
+  try {
+    // prepare filter, find an existing team that has same permalink
+    let filter: FilterQuery<ITeamDoc> = { permalink };
+
+    // in case edit team, try to find existing team that has same permalink
+    // and not current edited team
+    const _id = this.options.context?._id;
+    if (typeof _id !== 'undefined') {
+      filter._id = { $ne: _id };
+    }
+
+    team = await Team.findOne(filter);
+  } catch (error) {
+    // handle system error
+    const systemError = new BaseError(
+      `Error occurs when validate permalink`,
+      '',
+      500,
+      false
+    );
+
+    const nextFunc = this.options.context?.next;
+    if (typeof nextFunc !== 'undefined') {
+      nextFunc(systemError);
+    }
+    return false;
+  }
+
+  // if there is existing team with same permalink, return validation error
+  if (team) {
+    return false;
+  }
+
+  return true;
+};
+
+const duplicateNameValidation: TestFunction<
+  string | undefined,
+  Record<string, any>
+> = async function (this, name) {
+  if (typeof name === 'undefined') {
+    return true;
+  }
+
+  let team: (ITeamDoc & Document<any, any, ITeamDoc>) | null;
+  try {
+    // prepare filter, find an existing team that has same name
+    let filter: FilterQuery<ITeamDoc> = { name: name.toLowerCase() };
+
+    // in case edit team, try to find existing team that has same name
+    // and not current edited team
+    const _id = this.options.context?._id;
+    if (typeof _id !== 'undefined') {
+      filter._id = { $ne: _id };
+    }
+
+    team = await Team.findOne(filter);
+  } catch (error) {
+    // handle system error
+    const systemError = new BaseError(
+      `Error occurs when validate name`,
+      '',
+      500,
+      false
+    );
+
+    const nextFunc = this.options.context?.next;
+    if (typeof nextFunc !== 'undefined') {
+      nextFunc(systemError);
+    }
+    return false;
+  }
+
+  // if there is existing team with same name, return validation error
+  if (team) {
+    return false;
+  }
+
+  return true;
 };
 
 // NOTE:(message function - https://github.com/sideway/joi/blob/83092836583a7f4ce16cbf116b8776737e80d16f/test/base.js#L1920)
-export const teamFieldValidationSchema = joi.object<ITeamFieldForm, true>({
-  name: joi.string().required().label('Name'),
-  permalink: joi
-    .string()
+export const teamFieldValidationSchema: SchemaOf<ITeamFieldForm> = object({
+  name: string()
     .required()
-    .pattern(/^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/)
-    .label('Permalink'),
-  flagIcon: joi
-    .string()
+    .label('Name')
+    .test(
+      'duplicateNameValidation',
+      '${label} value is already existed',
+      duplicateNameValidation
+    ),
+  permalink: string()
+    .required()
+    .matches(/^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/)
+    .lowercase()
+    .label('Permalink')
+    .test(
+      'duplicatePermalinkValidation',
+      '${label} value is already existed',
+      duplicatePermalinkValidation
+    ),
+  flagIcon: string()
     .required()
     .label('Flag Icon')
-    .external(flagIconFilePathValidation),
-});
+    .test(
+      'flagIconFilePathValidation',
+      'Invalid ${label} file path',
+      flagIconFilePathValidation
+    ),
+  //TODO: add more async validation rule, check if all of them response at once
+}).defined();
 
-export const teamFileValidationSchema = joi.object<ITeamFileForm, true>({
-  flagIcon: joi.object().required().label('Flag Icon'),
+export const teamFileValidationSchema: SchemaOf<ITeamFileForm> = object({
+  // it's a workaround https://github.com/jquense/yup/pull/1358/files
+  flagIcon: mixed<Express.Multer.File>().required().label('Flag Icon'),
 });
