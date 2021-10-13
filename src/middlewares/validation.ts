b391f0c87627c89a4e611a92abad527674f37360
trungmt <trungtran.cs@gmail.com>
2021-10-14 03:22:14 +0700

    move validation flow inside upload middleware into validation schema

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -1,10 +1,8 @@
-// import Joi, { ValidationOptions } from 'joi';
 import { RequestHandler, Request, Response, NextFunction } from 'express';
 import { ValidationError } from '../exceptions/ValidationError';
-import yup, {
-  AnyObjectSchema,
-  ValidationError as yupValidationError,
-} from 'yup';
+import { AnyObjectSchema, ValidationError as yupValidationError } from 'yup';
+import { extname } from 'path';
+import BaseError from '../exceptions/BaseError';
 
 const options = {
   abortEarly: false,
@@ -18,26 +16,50 @@ export const validationAsync =
     const _id = req.params.id;
 
     try {
-      await schema.validate(req.body, {
+      await schema.validate(req, {
         ...options,
         context: { next, _id },
       });
       next();
       return;
     } catch (error) {
-      console.log('Error validationAsync', error);
       if (error instanceof yupValidationError) {
+        // console.log('Error validationAsync', error.inner);
         error.inner.forEach(err => {
-          const fieldName = err.path?.toString();
+          if (typeof err.path === 'undefined') {
+            return true;
+          }
+          const fieldName = getFieldNameFromPath(err.path);
           if (fieldName && req.errors && !req.errors[fieldName]) {
             req.errors[fieldName] = err.message;
           }
         });
+        const validationError = new ValidationError(
+          'There are input validation errors. Please check each input again base on our instruction.',
+          req.errors
+        );
+        next(validationError);
+        return;
       }
-      const validationError = new ValidationError(
-        'There are input validation errors. Please check each input again base on our instruction.',
-        req.errors
+      // console.log('Error validationAsync', error);
+      throw new BaseError(
+        'Error when running validation',
+        'Error when running validation',
+        500,
+        false
       );
-      next(validationError);
     }
   };
+
+/**
+ * Get form field name from yup validation error's path
+ *
+ * @param path string yup validation error path for each field
+ * @returns string in case path has form body.{form-field}, return form-field
+ *                 in case path has form {form-field}, return form-field
+ */
+const getFieldNameFromPath = (path: string | undefined): string | undefined => {
+  if (typeof path === 'undefined' || path === '') return undefined;
+  const fieldNameArr = extname(path).split('.');
+  return fieldNameArr.length === 1 ? path : fieldNameArr.pop()!;
+};
