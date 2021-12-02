dbd1175f425f041a72e665ee8614b55955e74d16
trungmt <trungtran.cs@gmail.com>
2021-12-02 21:30:38 +0700

    add cors, cookies parser and static folder uploads

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,46 @@
 import express from 'express';
+import cors from 'cors';
 import cron from 'node-cron';
+import cookieParser from 'cookie-parser';
 import authRouter from './routers/auth/auth';
 import adminRouter from './routers/admin/admin';
 import { logException, responseException } from './middlewares/exception';
 import { removeOldTempFiles } from './services/FileService';
+import path from 'path';
 require('./db/mongoose'); // move to providers/
 
 const app = express();
 
 // TODO: make it a provider class that inject response function to app object
 // same as https://github.com/GeekyAnts/express-typescript/blob/7bb6ac09ca0270cb82a1730d9bb8d6efb89c999e/src/middlewares/Kernel.ts
-const port = process.env.PORT || 8080;
+
+var dir = path.join(__dirname, '../uploads');
+console.log('dir', dir);
+
+app.use(express.static(dir));
+const corsConfig = {
+  // Configures the Access-Control-Allow-Origin
+  origin:
+    process.env.NODE_ENV !== 'production'
+      ? `http://localhost:3000`
+      : process.env.CORS_ORIGIN,
+
+  // Configures the Access-Control-Allow-Methods
+  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+
+  //Configures the Access-Control-Allow-Headers
+  allowedHeaders:
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization',
+
+  // Configures the Access-Control-Allow-Credentials
+  credentials: true,
+
+  //Configures the Access-Control-Expose-Headers
+  exposedHeaders: 'Content-Range,X-Content-Range,Authorization',
+
+  // Provides a status code to use for successful OPTIONS requests
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
 app.use(
@@ -18,6 +48,9 @@ app.use(
     extended: true,
   })
 );
+app.use(cors(corsConfig));
+app.use(cookieParser());
+
 app.use('/auth', authRouter);
 app.use('/api/admin', adminRouter);
 
