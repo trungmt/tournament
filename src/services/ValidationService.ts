ddc2f8c2af551603cd6b51f9c9a16982297ea852
trungmt <trungtran.cs@gmail.com>
2021-12-21 18:02:02 +0700

    feat<Tournament model>: add validation function related to Tournament model

--- src/services/ValidationService.ts
+++ src/services/ValidationService.ts
@@ -1,3 +1,16 @@
-export const validatePermalinkPattern = function (permalink: string) {
-  return /^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/.test(permalink);
+export const validatePermalinkPattern = (permalink: string): boolean =>
+  /^([a-zA-Z0-9]+-)*[a-zA-Z0-9]+$/.test(permalink);
+
+export const validateStageType = (type: StageType | null): boolean => {
+  if (type === null) return false;
+  return Object.values(StageType).includes(type);
 };
+
+export const isStageTypeRoundRobin = (type: StageType | null): boolean =>
+  type === StageType.RoundRobin;
+
+export const isStageTypeSingle = (type: StageType | null): boolean =>
+  type === StageType.SingleElimination;
+
+export const getStageTypeValidationMessage = (label: string): string =>
+  `${label} have to be one of these types: ${Object.keys(StageType).join(',')}`;
