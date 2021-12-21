cabe4007ccf8d8267cb96467908fe822c14b409e
trungmt <trungtran.cs@gmail.com>
2021-12-21 20:18:32 +0700

    refactor<Team Repository>: modify name of TeamRepository interface

--- src/repositories/admin/teams/TeamRepositoryInterface.ts
+++ src/repositories/admin/teams/TeamRepositoryInterface.ts
@@ -3,7 +3,7 @@ import { Document, HydratedDocument } from 'mongoose';
 import { PaginationResult } from '../../../services/PaginationService';
 
 export type ResultType = HydratedDocument<ITeamDoc, {}>;
-export default interface ITeamsRepository {
+export default interface TeamsRepositoryInterface {
   insertTeam(team: ITeamDoc): Promise<ResultType | null>;
   updateTeam(_id: string, team: ITeamDoc): Promise<ResultType | null>;
   deleteTeam(_id: string): Promise<ResultType | null>;
