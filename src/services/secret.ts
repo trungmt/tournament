bfb044af0e24d7d7c16e527c73503cec5635d8ee
trungmt <trungtran.cs@gmail.com>
2021-12-31 13:58:49 +0700

    refactor: remove secret file because there are configs file handle same purpose

--- src/services/secret.ts
+++
@@ -1,6 +0,0 @@
-import configs from '../configs';
-export const MONGOOSE_CONNECTION_URL = configs.mongooseConnectionURL;
-if (!MONGOOSE_CONNECTION_URL) {
-  console.log('Error: no MONGOOSE_CONNECTION_URL config');
-  process.exit(1);
-}
